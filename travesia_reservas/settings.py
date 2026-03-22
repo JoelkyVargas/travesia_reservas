1e344b408e69145a62f419c2248859874641943c
JoelkyVargas <joelky.vargas@gmail.com>
2026-03-22 13:01:36 -0600

    Use DATABASE_URL in production

--- travesia_reservas/settings.py
+++ travesia_reservas/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "travesia_reservas.wsgi.application"
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
